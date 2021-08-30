c8d5f58e8316fad0cf45d71cd2363db96b1adff1
Blake Nuttall <blakedyblake@gmail.com>
2021-08-30 14:59:05 -0600

    I made a test that's supposed to fail on purpose to see if circle ci can detect it

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+test('does the test come up in circle ci', ()=>{
+  expect((2).toBe(3))
+})
