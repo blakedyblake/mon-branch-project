e3085ccb9e8ac3fda5b2c5d060618e2579de42b6
Blake Nuttall <blakedyblake@gmail.com>
2021-08-30 15:02:41 -0600

    Forgot a parenthisis

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
 test('does the test come up in circle ci', ()=>{
-  expect((2).toBe(2))
+  expect((2)).toBe(2)
 })
